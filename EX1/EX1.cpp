5fb7c3e08747144176960bd4430bc251471e11ea
Andrey <89473462+Arty525@users.noreply.github.com>
2023-03-06 21:15:18 +0300

    Update EX1.cpp

--- EX1/EX1.cpp
+++ EX1/EX1.cpp
@@ -17,70 +17,70 @@ struct Film {
 };
 
 int main() {
-	Film move;
+	Film movie;
 	std::ofstream file("record.json");
 	//Green Mile//
-	move.title = "Green Mile";
-	move.year = 1999;
-	move.cast = {"Tom Hanks - Paul Edgecomb", "Mikle Clark Duncan - John Koffie", "David Mors - Brut <Brute> Howell", "Bonney Hunt  - Janise Edgecomb", "James Cromwell - Hall Murse"};
-	move.company = "Warner Brothers";
-	move.country = "USA";
-	move.screenwriter = { "Frank Darabont", "Stephen King"};
-	move.director = {"Frank Darabont"};
-	move.producer = { "Frank Darabont", "David Valdes"};
+	movie.title = "Green Mile";
+	movie.year = 1999;
+	movie.cast = {"Tom Hanks - Paul Edgecomb", "Mikle Clark Duncan - John Koffie", "David Mors - Brut <Brute> Howell", "Bonney Hunt  - Janise Edgecomb", "James Cromwell - Hall Murse"};
+	movie.company = "Warner Brothers";
+	movie.country = "USA";
+	movie.screenwriter = { "Frank Darabont", "Stephen King"};
+	movie.director = {"Frank Darabont"};
+	movie.producer = { "Frank Darabont", "David Valdes"};
 
 	nlohmann::json description = {
-		{"Title ", move.title},
-		{"Year", move.year},
-		{"Company", move.company},
-		{"Country", move.country},
-		{"Screenwriter", move.screenwriter},
-		{"Director", move.director},
-		{"Producer", move.producer},
-		{"Cast", move.cast}
+		{"Title ", movie.title},
+		{"Year", movie.year},
+		{"Company", movie.company},
+		{"Country", movie.country},
+		{"Screenwriter", movie.screenwriter},
+		{"Director", movie.director},
+		{"Producer", movie.producer},
+		{"Cast", movie.cast}
 	};
 	file << description << std::endl;
 	//The Terminal//
-	move.title = "The Terminal";
-	move.year = 2004;
-	move.cast = { "Tom Hanks - Victor Navorski", "Catrine Zeta-Jhones - Amelia Warren", "Mikle Noori - Maks", "Konrad Pla  - Officer", "Mark Ivanir - Taxi-driver Horan" };
-	move.company = "NONE";
-	move.country = "USA";
-	move.screenwriter = { "Jeff Natanson", "Andry Nikkol", "Sasha Jervacy"};
-	move.director = { "Stephen Spielberg" };
-	move.producer = { "Lory Mc.Donald", "Walter F. Parks", "Stephen Spielberg"};
+	movie.title = "The Terminal";
+	movie.year = 2004;
+	movie.cast = { "Tom Hanks - Victor Navorski", "Catrine Zeta-Jhones - Amelia Warren", "Mikle Noori - Maks", "Konrad Pla  - Officer", "Mark Ivanir - Taxi-driver Horan" };
+	movie.company = "NONE";
+	movie.country = "USA";
+	movie.screenwriter = { "Jeff Natanson", "Andry Nikkol", "Sasha Jervacy"};
+	movie.director = { "Stephen Spielberg" };
+	movie.producer = { "Lory Mc.Donald", "Walter F. Parks", "Stephen Spielberg"};
 
 	description = {
-		{"Title ", move.title},
-		{"Year", move.year},
-		{"Company", move.company},
-		{"Country", move.country},
-		{"Screenwriter", move.screenwriter},
-		{"Director", move.director},
-		{"Producer", move.producer},
-		{"Cast", move.cast}
+		{"Title ", movie.title},
+		{"Year", movie.year},
+		{"Company", movie.company},
+		{"Country", movie.country},
+		{"Screenwriter", movie.screenwriter},
+		{"Director", movie.director},
+		{"Producer", movie.producer},
+		{"Cast", movie.cast}
 	};
 
 	file << description << std::endl;
 	//Forrest Gamp//
-	move.title = "Forrest Gamp";
-	move.year = 1994;
-	move.cast = { "Tom Hanks - Forrest Gamp", "Robin Right - Jenny Carren", "Hary Sinise - lieutenant Den Taylor", "Sally Field - Mrs. Gamp", "Sam Anderson - Director Hankok"};
-	move.company = "NONE";
-	move.country = "USA";
-	move.screenwriter = { "Erik Rot", "Winston Groome"};
-	move.director = { "Robert Zemekis" };
-	move.producer = { "Wandy Finerman", "Steave Starkie", "Steave Teesh" };
+	movie.title = "Forrest Gamp";
+	movie.year = 1994;
+	movie.cast = { "Tom Hanks - Forrest Gamp", "Robin Right - Jenny Carren", "Hary Sinise - lieutenant Den Taylor", "Sally Field - Mrs. Gamp", "Sam Anderson - Director Hankok"};
+	movie.company = "NONE";
+	movie.country = "USA";
+	movie.screenwriter = { "Erik Rot", "Winston Groome"};
+	movie.director = { "Robert Zemekis" };
+	movie.producer = { "Wandy Finerman", "Steave Starkie", "Steave Teesh" };
 
 	description = {
-		{"Title ", move.title},
-		{"Year", move.year},
-		{"Company", move.company},
-		{"Country", move.country},
-		{"Screenwriter", move.screenwriter},
-		{"Director", move.director},
-		{"Producer", move.producer},
-		{"Cast", move.cast}
+		{"Title ", movie.title},
+		{"Year", movie.year},
+		{"Company", movie.company},
+		{"Country", movie.country},
+		{"Screenwriter", movie.screenwriter},
+		{"Director", movie.director},
+		{"Producer", movie.producer},
+		{"Cast", movie.cast}
 	};
 
 	file << description;
